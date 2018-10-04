dbf689d48d8f85a1f4786bb16c164039eb9efcc8
Paul Bernitz <paulbernitz@web.de>
2018-10-04 14:33:26 +0200

    Fixed formatting.

--- test/test-shell.js
+++ test/test-shell.js
@@ -1,31 +1,33 @@
 "use strict";
 
-const TinyShell = require("../src/shell.js");
+const TinyShell = require("../src/shell/shell.js");
 const Assert = require("assert");
 const Expect = require("chai").expect;
 const Should = require("chai").should();
 
-describe("TinyShell", function() {
-  describe("Test Prompt", function() {
-    it("prompt.write('done').read()", function() {
-      let out = new TinyShell().prompt.write("done").read();
+describe("Test TinyShell", function() {
+  describe("Prompt", function() {
+    it("Read", function() {
+      let out = new TinyShell(320, 240, 14, null).prompt
+        .write("done")
+        .rawBuffer();
       Expect(out).to.equal("done");
     });
 
-    it("prompt.write('done').flush().read()", function() {
+    it("Flush", function() {
       let out = new TinyShell().prompt
         .write("done")
         .flush()
-        .read();
+        .rawBuffer();
       Expect(out).to.be.empty;
     });
 
-    it("prompt.write('done').pop().pop().read()", function() {
+    it("Pop", function() {
       let out = new TinyShell().prompt
         .write("done")
         .pop()
         .pop()
-        .read();
+        .rawBuffer();
       Expect(out).to.equal("do");
     });
   });
